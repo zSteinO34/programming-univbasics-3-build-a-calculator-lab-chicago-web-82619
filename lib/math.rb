a4263d19abbb834261959fbe75278227733cf98e
Zac Steinberg <zstein1234@gmail.com>
2019-08-12 20:16:49 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,21 +3,21 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  puts num1 - num2
+  num1 - num2
 end
 
 def division(num1, num2)
-  puts num1 / num2
+  num1 / num2
 end
 
 def multiplication(num1, num2)
-  puts num1 * num2
+  num1 * num2
 end
 
 def modulo(num1, num2)
-  puts num1 % num2
+  num1 % num2
 end
 
 def square_root(num)
-  puts Math.sqrt(num)
+  Math.sqrt(num)
 end
